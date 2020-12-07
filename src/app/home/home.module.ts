645fb09f1181ae8476943fbf9dfdc01df49e8907
Najiba <najiba.r.halim@gmail.com>
2020-12-07 18:36:14 +0530

    Added plugin to call phone and added api call logic

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { CallNumber } from '@ionic-native/call-number/ngx';
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -14,6 +14,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [CallNumber]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { CallNumber } from '@ionic-native/call-number/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  online = true;
+  orders = [];
+  constructor(private call: CallNumber) {}
 
-  constructor() {}
+  ngOnInit(){
+    fetch("http://demo8360259.mockable.io/clients").then(response => response.json())
+      .then(data => this.orders = data.orders);
+  }
+
+  callNumber(phone) {
+    this.call.callNumber(phone, true)
+      .then(res => console.log('Launched dialer!', res))
+      .catch(err => console.log('Error launching dialer', err));
+    console.log(phone);
+  }
 
 }
